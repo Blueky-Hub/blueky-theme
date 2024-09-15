dd3cc3c2b094b6fdfe8c2fcabd1e74b77f91e273
Hoang Hxn <86338889+nguyenquanghoang@users.noreply.github.com>
2024-09-15 20:45:09 +0700

    Chuyển Repository cá nhân qua
    Tất cả file cũ của Blueky Theme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# blueky-theme
+# blueky-child-theme
+Blueky child theme (theme blueky) chính là tên gọi của giao diện mà Blueky đang sử dụng, được xây dựng dựa trên nền tảng Blocksy. 
---
+++ functions.php
@@ -0,0 +1,101 @@
+<?php
+if (! defined('WP_DEBUG')) {
+	die( 'Direct access forbidden.' );
+}
+add_action( 'wp_enqueue_scripts', function () {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+});
+
+require_once dirname(__FILE__) . '/tgmpa/class-tgm-plugin-activation.php';
+
+function my_theme_tgmpa_register_languages() {
+    $locale = determine_locale();
+    $locale = apply_filters('plugin_locale', $locale, 'tgmpa');
+
+    $mofile = dirname(__FILE__) . '/tgmpa/languages/tgmpa-' . $locale . '.mo';
+
+    load_textdomain('tgmpa', $mofile);
+}
+add_action('init', 'my_theme_tgmpa_register_languages');
+
+/**
+ * Đăng ký các plugin cần thiết cho theme Blueky Child
+ */
+function my_theme_register_required_plugins() {
+    $plugins = array(
+        array(
+            'name'      => '1. Forminator Forms',
+            'slug'      => 'forminator',
+            'required'  => false,
+        ),
+        array(
+            'name'      => '2. All-in-One WP Backup',
+            'slug'      => 'all-in-one-wp-migration',
+            'required'  => false,
+        ),
+        array(
+            'name'      => '3. Admin and Site Enhancements (ASE)',
+            'slug'      => 'admin-site-enhancements',
+            'required'  => false,
+        ),
+        array(
+            'name'      => '4. LiteSpeed Cache',
+            'slug'      => 'litespeed-cache',
+            'required'  => false,
+        ),
+        array(
+            'name'      => '5. Independent Analytics',
+            'slug'      => 'independent-analytics',
+            'required'  => false,
+        ),
+        array(
+            'name'      => '6. Google Site Kit',
+            'slug'      => 'google-site-kit',
+            'required'  => false,
+        ),
+        array(
+            'name'      => '7. Slim SEO',
+            'slug'      => 'slim-seo',
+            'required'  => false,
+        ),
+        array(
+            'name'      => '8. Defender Security',
+            'slug'      => 'defender-security',
+            'required'  => false,
+        ),
+    );
+
+    $config = array(
+        'id'           => 'tgmpa',
+        'default_path' => '',
+        'menu'         => 'tgmpa-install-plugins',
+        'parent_slug'  => 'themes.php',
+        'capability'   => 'edit_theme_options',
+        'has_notices'  => true,
+        'dismissable'  => true,
+		'dismiss_msg'  => '',
+        'is_automatic' => true,
+		'message'      => '',
+		'strings'      => array(
+			'page_title'   => __( 'Cài đặt các plugin cần thiết cho Blueky Child Theme', 'theme-slug' ),
+			'menu_title'   => __( 'Cài đặt các plugin', 'theme-slug' ),
+			'dismiss'  => __( 'Tắt thông báo', 'tgmpa' ),
+			// 'notice_can_install_recommended'  => _n_noop(
+            //     'Giao diện Blueky Child yêu cầu cài đặt plugin sau: %1$s.',
+            //     'Giao diện Blueky Child yêu cầu cài đặt plugin sau: %1$s.'
+            // ),
+		)
+    );
+
+    tgmpa($plugins, $config);
+}
+add_action('tgmpa_register', 'my_theme_register_required_plugins');
+
+// Funnelkit cron check
+define( 'BWF_CHECK_CRON_SCHEDULE', true );
+
+// Tắt chế độ toàn màn hình của Editor
+function jba_disable_editor_fullscreen_by_default() {
+	$script = "window.onload = function() { const isFullscreenMode = wp.data.select( 'core/edit-post' ).isFeatureActive( 'fullscreenMode' ); if ( isFullscreenMode ) { wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'fullscreenMode' ); } }";
+	wp_add_inline_script( 'wp-blocks', $script );
+}
